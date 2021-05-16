b3fa44b4dd82a13414f7234536bf7b6d2faeaa3a
Aayush Kumar <r3ptoid1@gmail.com>
2021-05-17 01:38:38 +0530

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,13 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
+          // child: Center(
           child: Column(
+            // mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
-                backgroundColor: Colors.red,
                 backgroundImage: AssetImage('images/me.jpeg'),
               ),
               Text(
@@ -30,14 +32,74 @@ class MyApp extends StatelessWidget {
               Text(
                 "FULL STACK DEVELOPER",
                 style: GoogleFonts.sourceSansPro(
-                  fontSize: 40.0,
+                  fontSize: 20.0,
                   color: Colors.white,
                   letterSpacing: .5,
-                  fontWeight: FontWeight.bold,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                elevation: 10.0,
+                // padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 25.0,
+                      ),
+                      Text(
+                        "+91 123 456 789",
+                        style: GoogleFonts.sourceSansPro(
+                          fontSize: 18.0,
+                          color: Colors.teal.shade900,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                elevation: 10.0,
+                // padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(0.0),
+                  child: ListTile(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0.0, horizontal: 10.0),
+                    leading: Icon(
+                      Icons.email_rounded,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "kaayush.ak71@gmail.com",
+                      style: GoogleFonts.sourceSansPro(
+                        fontSize: 18.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
           ),
+          // ),
         ),
       ),
     );
